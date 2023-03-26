0eeaf21bc653d89a4e87a87e0bdd31c6d53925b0
Yuuki-akiko06 <82251860+Yuuki-akiko06@users.noreply.github.com>
2023-03-26 18:14:00 +0800

    Update patch_qudao.sh

--- patch_qudao.sh
+++ patch_qudao.sh
@@ -12,7 +12,7 @@ download_azurlane () {
     # 使用curl命令下载apk文件
     #curl -o blhx.apk  $url
     
-    url="https://github.com/liusj5257/azurlane_anti_name/releases/download/%E5%BD%93%E4%B9%90%E6%B8%A0%E9%81%93/dangle.AzurLane.patched.apk"
+    url="https://download.cowcs.com/cowtransfer/cowtransfer/84809/ad0f1ab75aa64a12a79ee2fe6e8151ab.apk?auth_key=1679852296-fcd7753f20744f0e930518fe98c9504f-0-5a3b56cdd14e4ef8713064f926668eca&biz_type=1&business_code=COW_TRANSFER&channel_code=COW_CN_WEB&response-content-disposition=attachment%3B%20filename%3D23214782.apk%3Bfilename*%3Dutf-8%27%2723214782.apk&user_id=1023565177469684809&x-verify=1"
     curl -o blhx.apk -L $url
     
     fi
